138935cc50671758cbf02065e2cb3b8cda54fb3d
mswanson-mscst <mswanson.mscst@gmail.com>
2021-06-28 13:42:52 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSnackBarModule } from '@angular/material/snack-bar';
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+// import { MatListModule } from '@angular/material/list';
+// import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule} from '@angular/material/toolbar';
+// import { MatTooltipModule } from '@angular/material/tooltip'; 
+
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatMenuModule,
+    MatGridListModule, 
+    // MatFormFieldModule, MatInputModule,
+    // MatDialogModule, MatSelectModule, MatSnackBarModule,
+    // MatButtonToggleModule, MatListModule, MatAutocompleteModule, MatSlideToggleModule, 
+    // MatTooltipModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
